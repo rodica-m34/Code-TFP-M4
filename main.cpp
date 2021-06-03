bb5cd70342cc04adb913cedc2cf7350501839c5e
Rodica Munteanu <rodica_munteanu@live.com>
2021-06-03 14:32:25 +0300

    update

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 
     cout << "Hello there user !";
-    cout << "Welcome back !";
+    cout << "Welcome back gdfg!";
     cout << "This is an improved version of the application !";
 
     return 0;
